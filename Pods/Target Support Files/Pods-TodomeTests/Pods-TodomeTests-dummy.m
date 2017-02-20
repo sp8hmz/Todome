87fcade39063b7b39564ac71a3b2aaedc72a2f43
Karol Karczewski <sp8hmz@wp.pl>
2017-02-21 00:22:41 +0100

    .gitignore updated

--- Pods/Target Support Files/Pods-TodomeTests/Pods-TodomeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TodomeTests : NSObject
-@end
-@implementation PodsDummy_Pods_TodomeTests
-@end
--- Pods/Target Support Files/Pods-TodomeUITests/Pods-TodomeUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TodomeUITests : NSObject
-@end
-@implementation PodsDummy_Pods_TodomeUITests
-@end
